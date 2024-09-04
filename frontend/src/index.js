76b94c8e5e5fdd327f9c35da2d72a64a32726052
arbindras <157949417+arbindras@users.noreply.github.com>
2024-09-04 19:41:47 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="https://chat-app-umber-pi.vercel.app"
+export const BASE_URL="https://chat-app-u576.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
