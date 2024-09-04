6da32117a2dde499ad51cc1a148eb5beab30a703
arbindras <157949417+arbindras@users.noreply.github.com>
2024-09-04 13:12:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:3000",
+  origin: "https://chat-app-frontend-16sxfgnx6-arbindras-projects.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOption));
